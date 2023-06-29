f4b241a3807e49100ca81d7118d8f05f7f58ae5a
amatsuuu <andrematsumoto02@usp.br>
2023-06-29 13:48:52 -0300

    Update interface e tratamento de erros de entrada

--- App/main.py
+++ App/main.py
@@ -20,10 +20,10 @@ USER_PASSWORD = os.getenv("USER_PASSWORD")
 print("Tentando conexão...")
 connection = oracledb.connect(user=USERNAME, password=USER_PASSWORD, host="orclgrad1.icmc.usp.br", port=1521, service_name="pdb_elaine.icmc.usp.br")
 print("Conexão estabelecida!")
+print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>", end="\n\n")
 
 # Tirar as chamadas recursivas
 def main_menu():
-    clear()
     print(">>>>>>>> Meu Notebook, Minha Vida <<<<<<<<")
     print("Bem-vindo(a) à Página Inicial do nosso Sistema!")
     print("Escolha uma opção:")
@@ -35,16 +35,19 @@ def main_menu():
     option = input("Digite o número da opção desejada: ")
 
     if option == "1":
+        clear()
         cadastro_dispositivo()
     elif option == "2":
+        clear()
         perform_action()
     elif option == "3":
         connection.close()
         clear()
         exit()
     else:
+        clear()
         print("Opção inválida. Por favor, escolha uma opção válida.")
-        main_menu()
+        print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>", end="\n\n")
 
 
 def cadastro_dispositivo():
@@ -59,29 +62,39 @@ def cadastro_dispositivo():
             tipo = input("Tipo: ")
             modelo = input("Modelo: ")
             empresa = input("Empresa: ")
+            clear()
 
             cursor.execute("insert into dispositivo values (:numero_serial, :tipo, :modelo, 'DISPONIVEL', 1, :empresa)", 
                            [numero_serial, tipo, modelo, empresa])
 
-        except oracledb.IntegrityError as e:
-            error_obj, = e.args
-            print("ID já cadastrado.")
-            print("Error Code:", error_obj.code)
-            print("Error Full Code:", error_obj.full_code)
-            print("Error Message:", error_obj.message)
-            connection.rollback()
-            #clear()
-            print("")
-
         except oracledb.Error as e:
             error_obj, = e.args
-            print("Customer ID already exists")
-            print("Error Code:", error_obj.code)
-            print("Error Full Code:", error_obj.full_code)
-            print("Error Message:", error_obj.message)
+
+            if error_obj.code == 12899:
+                print("Valor da entrada excede o tamanho máximo.")
+            elif error_obj.code == 1400:
+                print("O número serial não pode ser nulo.")
+            elif error_obj.code == 2291:
+                print("Empresa não encontrada.")
+            
+            #print("Error Code:", error_obj.code)
+            #print("Error Context:", error_obj.context)
+            #print("Error Full Code:", error_obj.full_code)
+            #print("Error Message:", error_obj.message)
+            print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>", end="\n\n")
             connection.rollback()
 
+            acao = input("Você ainda deseja inserir um dispositivo? Se sim, digite 'S': ")
+            print("acao:", acao)
+            clear()
+            if acao.upper() != 'S':
+                return
+
+
+
         else:
+            print ("Dispositivo adicionado com sucesso!")
+            print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>", end="\n\n")
             connection.commit()
             return
 
@@ -110,4 +123,6 @@ def clear():
 if __name__ == "__main__":
     # connection = cx_Oracle.connect(user=USERNAME, password=USER_PASSWORD, dsn="pdb_elaine.icmc.usp.br/Pratica")
     # cursor = connection.cursor()
-    main_menu()
+    clear()
+    while(True):
+        main_menu()
